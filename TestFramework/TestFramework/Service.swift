f7a84b1cbedd6c7eb8b51ed3518fe1f582ed100e
Nihanth <nihanth.gajam94@gmail.com>
2019-05-18 17:20:20 -0400

    initial commit

---
+++ TestFramework/TestFramework/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  TestFramework
+//
+//  Created by Nihanth Gajam on 5/18/19.
+//  Copyright © 2019 Nihanth Gajam. All rights reserved.
+//
+
+import Foundation
+
+
+public class Service {
+    
+    private init(){}
+    
+    public static func doStuff() ->String{
+        return "i am doing some stuff...."
+    }
+}
